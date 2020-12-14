6e0540c77d5c39f227d9e3769b72a07862d7440e
DanSoriano <76001619+DanSoriano@users.noreply.github.com>
2020-12-15 00:44:18 +0800

    Add files via upload

--- Test.java
+++ Test.java
@@ -1,18 +1,37 @@
-  
-
 public class Test {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		AVLTree t = new AVLTree();
-		t.root = t.insert(t.root, 10);
-		t.root = t.insert(t.root, 20);
-		t.root = t.insert(t.root, 30);
-		t.root = t.insert(t.root, 40);
-		t.root = t.insert(t.root, 50);
-		t.root = t.insert(t.root, 25);
-		t.preOrder(t.root);
-
+		GraphAL d = new GraphAL(6);
+		
+		d.addEdge(1, 2);
+		d.addEdge(1, 4);
+		d.addEdge(2, 4);
+		d.addEdge(2, 1);
+		d.addEdge(3, 3);
+		d.addEdge(3, 5);
+		d.addEdge(4, 5);
+		d.addEdge(5, 3);
+	
+		d.displayGraph();
+		
+	
+		GraphAM g = new GraphAM(6);
+		
+		g.addEdge(1, 2);
+		g.addEdge(1, 4);
+		g.addEdge(2, 4);
+		g.addEdge(2, 1);
+		g.addEdge(3, 3);
+		g.addEdge(3, 5);
+		g.addEdge(4, 5);
+		g.addEdge(5, 3);
+		
+		g.displayGraph();
+		
+		System.out.print("BFS: ");
+		d.BFS(1);
+		System.out.println();
+		System.out.print("DFS: ");
+		d.DFS(1);	
 	}
-
 }
